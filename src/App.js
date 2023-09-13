249715d45b3f727ce9f116abbf605fa5427d04c1
[OgumaMomoka] <[oguma.momoka.kb6.aws@gmail.com]>
2023-09-13 08:09:14 +0000

    テストデプロイ

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reacttttttttttt
+          LEARN REACT
         </a>
       </header>
     </div>
